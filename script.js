dd937f177b0c07342bae7e110b8d4522ab5f60d4
Menenn <jpgrapeiaa@gmail.com>
2021-01-20 18:21:47 -0300

    Begginers03

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleDarkMode() {
+    if (document.body.classList.value != "dark-mode"){
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+}
